b485344545f84e662848f144b6bbcf16697fb90d
Ayoubhm1993 <Ayoubhm1993@gmail.com>
2020-10-07 08:15:20 -0500

    add loop and array

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,14 +2,12 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   // Alert users that they are starting the round
 // if player choses to fight, then fight
@@ -61,4 +59,15 @@ else if (promptFight === "skip" || promptFight === "SKIP") {
 }
 
 };
-fight()
+
+console.log(enemyNames);
+console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
